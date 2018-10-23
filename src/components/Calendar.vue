928f33a6a29773193c70f826d350ff414c6eede0
rubenberna <rubenmbernardes@gmail.com>
2018-10-23 15:18:09 +0200

    feat(): render fullcalendar in calendar component

---
+++ src/components/Calendar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="calendar">
+    <el-container>
+      <el-header>Meeting Rooms</el-header>
+      <el-main>
+        <full-calendar :events="events"
+                       :config="config"/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  import { FullCalendar } from 'vue-full-calendar'
+
+  export default {
+    name: 'calendar',
+    data() {
+     return {
+       events: [
+         {
+             title  : 'event1',
+             start  : '2010-01-01',
+         },
+         {
+             title  : 'event2',
+             start  : '2010-01-05',
+             end    : '2010-01-07',
+         },
+         {
+             title  : 'event3',
+             start  : '2010-01-09T12:30:00',
+             allDay : false,
+         },
+       ],
+       config: {
+         weekends: false,
+         editable: true,
+         selectHelper: true
+       }
+     }
+   },
+    components: {
+      FullCalendar
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../../style/main.scss';
+
+  #calendar {
+    max-width: 1125px;
+    max-height: 80%;
+    margin: 0 auto;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FullCalendar from 'vue-full-calendar'
+import 'fullcalendar/dist/fullcalendar.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(FullCalendar)
+Vue.use(ElementUI)
 
 new Vue({
   router,
