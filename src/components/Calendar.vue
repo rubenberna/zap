0011ff24f0b170e5679b005be9d00cb5b0c7c55a
rubenberna <rubenmbernardes@gmail.com>
2018-10-29 17:22:51 +0100

    style(): rearranged input size and spacing for both dialogs

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -11,31 +11,29 @@
                        @event-created='select'/>
       </el-main>
     </el-container>
+
     <el-dialog
       title="New event"
       :visible.sync="dialogVisible"
-      width="30%">
-      <div class="book-room-start">
-        <div class="block">
-           <span class="demonstration">Start</span>
+      width="30%"
+      class="calendar-modal">
+      <div class="calendar-input">
+           <span>Start</span>
            <el-date-picker
              v-model="booking.start"
              type="datetime"
              disabled>
            </el-date-picker>
-         </div>
       </div>
-      <div class="book-room-end">
-        <div class="block">
+      <div class="calendar-input">
            <span class="demonstration">End</span>
            <el-date-picker
              v-model="booking.end"
              type="datetime"
              disabled>
            </el-date-picker>
-         </div>
       </div>
-      <div class="book-room-title">
+      <div class="calendar-input">
         <span>Name</span>
         <el-input placeholder="Title of booking" v-model="booking.title"></el-input>
       </div>
@@ -44,12 +42,17 @@
         <el-button type="primary" @click.prevent="setBooking(booking)">Confirm</el-button>
       </span>
     </el-dialog>
+
+    <!-- <calendar-dialog :booking='booking'
+                     :dialogVisible='dialogVisible'
+                     :disabled='disabled' /> -->
   </div>
 </template>
 
 <script>
   import { FullCalendar } from 'vue-full-calendar'
-  import { mapGetters } from 'vuex'
+  import { mapGetters, mapActions } from 'vuex'
+  import CalendarDialog from '@/components/dialogs/CalendarDialog'
 
   export default {
     name: 'calendar',
@@ -70,6 +73,7 @@
          minTime: '07:00:00'
        },
        dialogVisible: false,
+       disabled: false,
        booking: {
          start: null,
          end: null,
@@ -78,19 +82,28 @@
      }
    },
    components: {
-     FullCalendar
+     FullCalendar,
+     CalendarDialog
    },
     computed: {
       ...mapGetters(['reservations', 'pickedRoom']),
     },
     methods: {
+      ...mapActions(['createBooking']),
       toggleDialog() {
         this.dialogVisible = !this.dialogVisible
       },
       select(start, end) {
         this.booking.start = start.start._d
         this.booking.end = start.end._d
+
+        console.log(start);
         this.dialogVisible = true
+        this.disabled = true
+      },
+      setBooking(booking) {
+        this.createBooking(booking)
+        this.dialogVisible = false
       }
     }
   }
@@ -105,6 +118,15 @@
       height: 459px;
       overflow: hidden;
     }
+  }
+
+  .calendar-input {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 45%;
+    margin-bottom: 14px;
+    margin-left: 20px;
   }
 
 </style>
---
+++ src/components/dialogs/CalendarDialog.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="calendar-booking">
+    <el-dialog
+      title="New event"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <div class="book-room-start">
+        <div class="block">
+           <span class="demonstration">Start</span>
+           <el-date-picker
+             v-model="booking.start"
+             type="datetime"
+             disabled>
+           </el-date-picker>
+         </div>
+      </div>
+      <div class="book-room-end">
+        <div class="block">
+           <span class="demonstration">End</span>
+           <el-date-picker
+             v-model="booking.end"
+             type="datetime"
+             disabled>
+           </el-date-picker>
+         </div>
+      </div>
+      <div class="book-room-title">
+        <span>Name</span>
+        <el-input placeholder="Title of booking" v-model="booking.title"></el-input>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="toggleDialog">Cancel</el-button>
+        <el-button type="primary" @click.prevent="setBooking(booking)">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'calendar-booking',
+  // data() {
+  //   return {
+  //     dialogVisible: false
+  //   }
+  // },
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      default: false
+    },
+    booking: {
+      type: Object
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    toggleDialog() {
+      this.dialogVisible = !this.dialogVisible
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
