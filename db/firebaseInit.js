a98deccd5773ba1b482db990fb1ff9f62c28e398
Ruben Bernardes <ruben@redcarrots.be>
2019-01-22 17:13:39 +0100

    chore(): removed initialization of firebase

--- db/firebaseInit.js
+++ db/firebaseInit.js
@@ -1,21 +1,21 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
-import firebaseConfig from './firebaseConfig'
-
-firebase.initializeApp(firebaseConfig)
-
-const db = firebase.firestore()
-
-db.settings({
-  timestampsInSnapshots: true
-})
-
-// Shortcuts for db collections
-const meetingRooms = db.collection('meeting_rooms')
-const reservations = db.collection('reservations')
-
-export default {
-  db,
-  meetingRooms,
-  reservations
-}
+// import firebase from 'firebase'
+// import 'firebase/firestore'
+// import firebaseConfig from './firebaseConfig'
+//
+// firebase.initializeApp(firebaseConfig)
+//
+// const db = firebase.firestore()
+//
+// db.settings({
+//   timestampsInSnapshots: true
+// })
+//
+// // Shortcuts for db collections
+// const meetingRooms = db.collection('meeting_rooms')
+// const reservations = db.collection('reservations')
+//
+// export default {
+//   db,
+//   meetingRooms,
+//   reservations
+// }
